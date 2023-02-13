57ded80eebe52d9a70ecf665d8a36d32cdd55e5b
Under4u <89784115+Under4u@users.noreply.github.com>
2023-02-13 10:12:02 +0000

    les1_test1

---
+++ app.js
@@ -0,0 +1,12 @@
+const puppeteer = require('puppeteer');
+
+async function run(){
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://www.github.com');
+
+    await page.screenshot({path: 'example.png', fullPage: true});
+    await browser.close();
+}
+
+run();
