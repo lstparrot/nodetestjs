1bfbcbecd45901a5cf0561bc13bd0064741bc587
Under4u <89784115+Under4u@users.noreply.github.com>
2023-02-14 13:28:27 +0000

    less10

---
+++ appnext.js
@@ -0,0 +1,50 @@
+/*for(var i = 0; i < 10; i++){
+    console.log(i);
+}
+
+for(var i = 100; i > 5; i /= 2){
+    console.log(i);
+}
+*/
+
+/*
+var j = 1000;
+while(j>=100){
+    j -= 100;
+    console.log(j);
+}
+*/
+
+/*var test = true;
+while(test){
+
+}*/
+
+
+/*
+var x = 0;
+do{
+    console.log(x);
+    x++;
+} while(x<10);
+*/
+
+/*
+
+for(var i = 10; i<=20; i+= 2){
+    if(i>15)
+    break;
+    console.log(i);
+}
+
+for(var i = 10; i<=20; i++){
+    if(i % 2 ==0)
+    continue;
+    console.log(i);
+}
+*/
+
+var arrr = [5,3,34,545];
+for(var i = 0; i< arrr.length; i++){
+    console.log(i + " " + arrr[i]);
+}
