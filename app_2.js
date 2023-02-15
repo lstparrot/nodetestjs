61d85e800e3126ce7167985b433433fd3f0b6449
Under4u <89784115+Under4u@users.noreply.github.com>
2023-02-15 10:31:50 +0000

    l_10

---
+++ app_2.js
@@ -0,0 +1,18 @@
+/*alert("Hello");
+var date = confirm("test");
+console.log(date);
+
+
+prompt("How cow?", 20);
+var date2 = prompt("How cow?", 20);*/
+
+var person = null;
+if(confirm("Your ready: ")){
+    if(person = prompt("Name")){
+    alert("Name: " + person);
+    } else {
+        alert("Your press NO");
+    }
+} else {
+    alert("Your press NO");
+}
